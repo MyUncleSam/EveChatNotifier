3e8695ff6b1520aa031a99b5bfe176edfef0244e
Stefan Ruepp <s.ruepp@lobodms.com>
2018-01-18 22:31:16 +0100

    fixed wrong version number

--- EveChatNotifier/Properties/AssemblyInfo.cs
+++ EveChatNotifier/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.8.3.0")]
-[assembly: AssemblyFileVersion("2.8.3.0")]
+[assembly: AssemblyVersion("2.8.4.0")]
+[assembly: AssemblyFileVersion("2.8.4.0")]
